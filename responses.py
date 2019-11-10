c01945a4358f0a916a205361d1215c428c910025
theriley106 <christopherlambert106@gmail.com>
2019-11-09 17:39:51 -0800

    updated responses

--- responses.py
+++ responses.py
@@ -1,6 +1,8 @@
 def start_response():
 	# This is the speech that is said when the skill starts for the first time
-	return "Hello World!"
+	return """Thank you for checking out Book Me.  An Amazon Alexa skill that can 
+	book reservations at any local restaraunt.
+	"""
 
 def what_day():
 	# This is the speech that is said when the person asks what day the event starts
