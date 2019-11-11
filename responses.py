966170173d548f34b5610a18c8b5b0a25e689d4b
theriley106 <christopherlambert106@gmail.com>
2019-11-11 08:57:21 -0800

    Added responses

--- responses.py
+++ responses.py
@@ -4,6 +4,10 @@ def start_response():
 	book reservations at any local restaurant -- even those without online booking systems.
 	"""
 
+def what_can_i_buy():
+	return """Book me will make a phone call to the restaurant on your behalf -- this is a premium service
+	that costs 99 cents"""
+
 def what_day():
 	# This is the speech that is said when the person asks what day the event starts
 	return "Hello world will take place on October 10th"
